3672063474de983db866f56bd32b9de7cd31730f
JeffGillispie <JGillisp@asu.edu>
2016-12-05 16:08:56 -0600

    Resolved compile error.

--- src/test/java/parsers/TU_TextDelimitedParser.java
+++ src/test/java/parsers/TU_TextDelimitedParser.java
@@ -1,12 +1,16 @@
 package parsers;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import eAdapter.Document;
+import eAdapter.Representative;
 import parsers.ParserArguments;
 import parsers.TextDelimitedParser;
 
@@ -24,8 +28,10 @@ public class TU_TextDelimitedParser {
         Scanner scanner = new Scanner("one,two,buckle,my,shoe");
         Delimiters delimiters = Delimiters.COMMA_DELIMITED;
         String[] header = new String[] { "1", "2", "3", "4", "5" };
+        List<String> fields = Arrays.asList(header);
+        List<Triple<String, String, Representative.Type>> reps = null;
         ParserArguments args = ParserArguments.of(scanner, delimiters, header);
-        Document doc = parser.parseDocument(args);
+        Document doc = parser.parse(scanner, delimiters, fields, "1", reps);
         Assert.assertEquals("one", doc.getMetadata().get("1"));
         Assert.assertEquals("two", doc.getMetadata().get("2"));
         Assert.assertEquals("buckle", doc.getMetadata().get("3"));
